710ebe1d3ad176654b5025465947e9c4d5662289
Pavel Tolkach <pb.tolkach@outlook.com>
2022-11-08 13:04:30 +0300

    Добавьте файлы проекта.

---
+++ Task1/BankAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class BankAccount<T>
+    {
+        T number;
+        double balance;
+        string name;
+
+        public T Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+            }
+        }
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                balance = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string GetInfo()
+        {
+            return $"{Number} Остаток на счете: {Balance}млн$ ФИО: {Name}";
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<int> BA1 = new BankAccount<int>();
+            BA1.Number = 5000;
+            BA1.Balance = 18400;
+            BA1.Name = "Алишер Усманов";
+            Console.WriteLine(BA1.GetInfo());
+
+            BankAccount<string> BA2 = new BankAccount<string>();
+            BA2.Number = "A009";
+            BA2.Balance = 17200;
+            BA2.Name = "Павел Дуров";
+            Console.WriteLine(BA2.GetInfo());
+
+            BankAccount<double> BA3 = new BankAccount<double>();
+            BA3.Number = 16.5;
+            BA3.Balance = 11100;
+            BA3.Name = "Леонид Федун";
+            Console.WriteLine(BA3.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
